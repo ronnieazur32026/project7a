8bd60c976e5c4357fe960bd524578f7279f0730e
ronnieazur32026 <ronnieazur32026@outlook.com>
2026-02-14 09:29:19 -0600

    Update config.py

--- config.py
+++ config.py
@@ -38,6 +38,7 @@ class Config(object):
 
     # You can find the proper permission names from this document
     # https://docs.microsoft.com/en-us/graph/permissions-reference
-    SCOPE = ["User.Read"] # Only need to read user profile for this app
+    #SCOPE = ["User.Read"] # Only need to read user profile for this app
+    SCOPE = ["openid", "profile", "email"]
 
     SESSION_TYPE = "filesystem"  # Token cache will be stored in server-side session
